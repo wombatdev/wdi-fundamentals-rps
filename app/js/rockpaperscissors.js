f82b5f48e3dea8d43abf6f3e3c03669e22a69188
James <james@team-wombat.com>
2016-05-12 18:16:31 -0400

    removed submodule and re-uploading entire rps file

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,12 +1,11 @@
-////////////////////////////////////////////////
-/*   Provided Code - Please Don't Edit   */
-////////////////////////////////////////////////
-'use strict';
-
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
+    var choice = prompt("Please choose either 'rock', 'paper', or 'scissors'.","To quit enter 'quit'");
+    while (choice != "rock" && choice != "paper" && choice != "scissors" && choice != "quit") {
+      choice = prompt("You must enter 'rock', 'paper', or 'scissors'.","To quit enter 'quit'");
+    }
+    return choice;
 }
+
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
@@ -17,39 +16,149 @@ function randomPlay() {
         return "scissors";
     }
 }
-////////////////////////////////////////////////
-/*           Write Your Code Below            */
-////////////////////////////////////////////////
 
-function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+function getPlayerMove() {
+	var playerMove = getInput();
+	if (playerMove === "quit") {
+		return playerMove;
+	}
+	else {
+		console.log("You played "+playerMove+"!");
+		return playerMove;
+	}
 }
-
-function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+function getComputerMove() {
+	var computerMove = randomPlay();
+	console.log("The computer played "+computerMove+"!");
+	return computerMove;
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+	var winner;
+	playerMove = getPlayerMove();
+	if (playerMove === "quit") {
+		winner = "over";
+		return winner;
+	}
+	computerMove = getComputerMove();
+
+    if (computerMove === "rock" ) {
+      if (playerMove === "scissors" ) {
+        winner = "computer";
+      }
+      else if (playerMove === "paper") {
+        winner = "player";
+      }
+      else {
+        winner = "tie";
+      }
+    }
+    if (computerMove === "paper" ) {
+      if (playerMove === "rock" ) {
+        winner = "computer";
+      }
+      else if (playerMove === "scissors") {
+        winner = "player";
+      }
+      else {
+        winner = "tie";
+      }
+    }
+    if (computerMove === "scissors" ) {
+      if (playerMove === "paper" ) {
+        winner = "computer";
+      }
+      else if (playerMove === "rock") {
+        winner = "player";
+      }
+      else {
+        winner = "tie";
+      }
+    }
+    if (winner === "tie") {
+    	console.log("It's a tie!");
+    }
+    else if (winner === "player") {
+        console.log("You win!");
+    }
+    else {
+    	console.log("The computer wins!");
+    }
     return winner;
 }
 
-function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+function playToX() {
+    console.log("Let's play Rock, Paper, Scissors!");
+    var max = prompt("What score do you want to play to?","Must be a positive whole number");
+    while (max <= 0 || max%1 !== 0) {
+      max = prompt("You must enter a positive integer.","Positive integer here");
+    }
+	console.log("OK, let's get started!");
+
+	var closeCounter = 0;
+	var blowoutCounter = 0;
+	var tension = 0;
+
+	while (playerWins < max && computerWins < max) {
+		var winner = "tie";
+    	var winner = getWinner();
+		if (winner === "player") {
+			playerWins ++;
+		}
+		if (winner === "computer") {
+			computerWins ++;
+		}
+    	if (winner === "over") {
+      		break;
+    	}
+		if (playerWins == max) {
+		    console.log("Final score: You: "+playerWins+", Computer: "+computerWins+"!");
+		    break;
+		}
+		if (computerWins == max) {
+		    console.log("Final score: You: "+playerWins+", Computer: "+computerWins+"!");
+		    break;
+		}
+    	console.log(playerWins+", "+computerWins);
+    	if (max >= 3 && playerWins === max-1 && computerWins == max-1 && closeCounter === 0) {
+    		console.log("It's coming down to the wire!");
+    		var tension = 1;
+    		var closeCounter = 1;
+    	}
+    	if (max >= 4 && Math.abs(playerWins-computerWins) === max-1 && blowoutCounter === 0) {
+    		console.log("We're looking at a blowout, folks!");
+    		var comeback = "set";
+    		var blowoutCounter = 1;
+    	}
+    	if (comeback == "set" && Math.abs(playerWins-computerWins) === 0 ) {
+    		var comeback = "undone";
+    		console.log("WHAT A COMEBACK!")
+    	}
+    	if (tension >= 2) {
+    		switch (tension) {
+	    		case 2:
+			        console.log("This is a close one!");
+			        break;
+			    case 3:
+			        console.log("What a finish!");
+			        break;
+			    case 4:
+			        console.log("This is one for the ages!");
+			        break;
+			    case 5:
+			        console.log("ARE YOU FREAKING KIDDING ME???");
+			        break;
+			    default:
+			        break;
+	    	}
+    	}
+    	if (playerWins == computerWins && tension >= 1) {
+    		tension++;
+    	}
+	}
 }
 
+var playerWins = 0;
+var computerWins = 0;
+
+playToX();
